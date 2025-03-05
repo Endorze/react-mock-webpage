700001658f003a4700ec6cfb673f536833c582d7
Endorze <alexyurihallgren@hotmail.com>
2025-03-05 16:57:24 +0100

    feat: Reworked component to take props for reusability

--- src/components/GridTextImageSection/gridTextImageSection.jsx
+++ src/components/GridTextImageSection/gridTextImageSection.jsx
@@ -1,32 +1,29 @@
 import styles from "./gridTextImageSection.module.css";
 import Container from "../Container/container.jsx";
-import VerticalDisplay from "../VerticalDisplay/verticalDisplay.jsx";
 import Wrapper from "../Wrapper/wrapper.jsx";
-import Button from "../Button/button.jsx";
 
-const GridTextImageSection = ({title1, title2, title3, title4, description1, description2, imagePath, buttonText}) => {
+const GridTextImageSection = ({title1, title2, title3, title4, description1, description2, imagePath, buttonText, buttonIdNav}) => {
     return (<div>
         <Container>
             <Wrapper>
                 <div className={styles.section}>
                     <div className={styles.sectionDiv1}>
-                    <h3>Get to know Bethesda</h3>
-                    <p>We're more than just an Avarage Gaming Studio</p>
+                    <h3>{title1}</h3>
+                    <p>{title2}</p>
                     </div>
                     <div className={styles.sectionDiv2}>
                         <div className={styles.buttonDiv}>
-                            <h2>Eager to know more about us?</h2>
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus modi molestias dignissimos exercitationem ipsa iusto nulla aliquam minima explicabo tempore. Veritatis impedit voluptates eum illum reprehenderit quidem sequi qui quia?</p>
-                            <a href="#team" className={styles.gridButton}><h2>Meet our Crew</h2></a>
+                            <h2>{title3}</h2>
+                            <p>{description1}</p>
+                            <a href={`${buttonIdNav}`} className={styles.gridButton}><h2>{buttonText}</h2></a>
                         </div>
                     </div>
                     <div className={styles.sectionDiv3}>
-                    <img src="https://cdn1.epicgames.com/spt-assets/541b3ca52c4f423d83fb2411c5e45962/oblivion-thtkx.jpg" />
+                    <img src={`${imagePath}`} />
                     </div>
                     <div className={styles.sectionDiv4}>
-                        <h2>Why should you trust us?</h2>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veritatis exercitationem similique sequi iusto et debitis repellat. Exercitationem porro maiores eum aliquid, voluptatum incidunt deserunt quis ipsa voluptate sit enim consequatur?</p>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, quasi cumque sit similique nemo incidunt repudiandae ipsa tempora a, sequi vel pariatur quisquam quibusdam voluptas officia animi ullam quas in.</p>
+                        <h2>{title4}</h2>
+                        <p>{description2}</p>
                     </div>
                 </div>
             </Wrapper>
--- src/pages/Overview/overview.jsx
+++ src/pages/Overview/overview.jsx
@@ -4,12 +4,16 @@ import LocationSection from "../../components/LocationSection/locationSection";
 import GridTextImageSection from "../../components/GridTextImageSection/gridTextImageSection";
 import VideoSection from "../../components/VideoSection/videoSection";
 
-
 const Overview = () => {
     return (
         <div>
             <Divider />
-            <GridTextImageSection />
+            <GridTextImageSection title1={"Watch our new Series"} title2={"Our series just won an Emmy-Award. Watch it today! "} title3={"What is the series all about?"} title4={"Will there be a second season?"} description1={"The Fallout series is a post-apocalyptic RPG set in a world where 1950s retro-futurism meets nuclear devastation. Players explore vast wastelands, battle mutants and factions, and shape the world through choices. With deep storytelling, dark humor, and immersive survival mechanics, Fallout offers a gripping journey through a broken yet resilient world."} 
+            description2={"Fallout is officially renewed for Season 2 on Prime Video. Based on Bethesda's popular video game series of the same name, the post-apocalyptic story of life in America after nuclear war has captivated audiences. Following HBO's The Last of Us, it becomes the latest video game adaptation to score big with both fans and critics alike."}
+            buttonText={"Watch trailer"}
+            imagePath={"https://images.fallout.wiki/e/ee/FOTV_Marketing_Lucy_Poster.jpg"}
+            buttonIdNav={"#trailer"}
+            />
             <Divider />
             <VideoSection videoLink={"https://www.youtube.com/embed/0kQ8i2FpRDk?si=q0-x1pNcmF4-NtqH"} title1={"Fallout - The new Series"} title2={"Where can i watch Fallout?"} 
             description1={"Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis voluptates error itaque consectetur officia. Eaque voluptatem ipsa autem magni fuga pariatur doloremque, placeat corporis id, assumenda nam provident culpa expedita?"}
