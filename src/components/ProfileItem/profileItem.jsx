b4cec13836168d01bf66db86040dbac5a636685c
Endorze <alexyurihallgren@hotmail.com>
2025-03-06 16:02:18 +0100

    Adjusted some css in footer, profile section.

--- src/components/ProfileItem/profileItem.jsx
+++ src/components/ProfileItem/profileItem.jsx
@@ -1,13 +1,22 @@
 import styles from "./profileItem.module.css";
+import favoriteLogo from "/image/about/profile/sweden.png"
 
 const ProfileItem = ({imagePath, name, description}) => {
     return (
         <div className={styles.profileItem}>
             <div className={styles.imageDiv}>
-                <img src={`${imagePath}`}></img>
+                <div className={styles.countryFlag}>
+                    <img src={favoriteLogo} />
+                </div>
+                <img src={`${imagePath}`} />
             </div>
-            <h2>{name}</h2>
-            <p>{description}</p>
+            <div className={styles.horizontal}>
+                <div>
+                    <h2>{name}</h2>
+                    <p>Frontend Developer</p>
+                </div>
+                <p>{description}</p>
+            </div>
         </div>
     )
 }
